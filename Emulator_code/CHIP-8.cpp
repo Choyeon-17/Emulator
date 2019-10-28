73b5512c4ab05920bb0a43863b5066738088993f
Choyeon <VtKoHcAwSmKtV@gmail.com>
2019-10-28 23:02:47 +0900

    Update CHIP-8.cpp

--- Emulator_code/CHIP-8.cpp
+++ Emulator_code/CHIP-8.cpp
@@ -1,8 +1,23 @@
 #include <cstdio>
 #include <cstdint>
 
-unsigned char memory[4096] = { 0 }, V[16] = { 0 }, delay_timer, sound_timer;
+unsigned char V[16] = { 0 };
 unsigned short opcode = 0, I = 0, pc = 0x200, stack[16], sp = 0;
+
+unsigned char memory[4096] = 
+{
+    0x00, 0x00, 
+    0x00, 0x0E, 
+    0x20, 0x00, 
+    0x53, 0x00, 
+    0x13, 0x00
+};
+
+uint8_t ram[4] = 
+{
+    0x12, 0x00, 
+    0x27, 0x00
+};
 
 class Chip8
 {
@@ -19,20 +34,34 @@ public:
 				switch (opcode & 0x000F)
 				{
 				case 0x0000:
-					// Execute opcode
+					// 스크린을 비운다.
 					break;
 
 				case 0x000E:
-					// Execute opcode
+					// 서브 루틴으로부터 반환한다.
 					break;
 
 				default:
 					printf("Unknown opcode [0x0000]: 0x%X\n", opcode);
 				}
 				break;
-			case 0x1000:
-				// Execute opcode
+
+			case 0x2000:
+				stack[sp] = pc;
+                ++sp;
+                pc = opcode & 0x0FFF;
 				break;
+
+            case 0x5000:
+                printf("My opcode: %X\n", (ram[pc - 6] << 8) | (ram[pc - 4]));
+                break;
+
+            case 0x1000:
+                printf("Wizard\n");
+                break;
+
+            default:
+                printf("Unknown opcode [0x0000]: 0x%X\n", opcode);
 			}
 
 			pc += 2;
@@ -45,7 +74,6 @@ int main()
 	int timer;
 
 	// setupGraphics();
-	setupInput();
 
 	Chip8 Chip_8;
 	// Chip_8.loadGame("pong");
